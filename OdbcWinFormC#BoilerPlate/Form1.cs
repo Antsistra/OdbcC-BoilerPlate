53fde55682e62837c39b6110d14015aedb2c0eff
Hadinmd <hadinurmuhamad8@gmail.com>
2023-07-29 22:23:45 +0700

    init

--- OdbcWinFormC#BoilerPlate/Form1.cs
+++ OdbcWinFormC#BoilerPlate/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Odbc;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,17 @@ namespace OdbcWinFormC_BoilerPlate
 {
     public partial class Form1 : Form
     {
+        OdbcCommand dml;
+        OdbcDataReader dr;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ OdbcWinFormC#BoilerPlate/connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdbcWinFormC_BoilerPlate
+{
+    internal class connection
+    {
+        OdbcCommand dml;
+
+        public static OdbcDataReader dr;
+        public static OdbcConnection conn;
+
+        public void OpenConn()
+        {
+            conn = new OdbcConnection("DSN=name+db");
+            conn.Open();
+        }
+    }
+}
